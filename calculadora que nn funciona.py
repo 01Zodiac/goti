c69f96089e4f9279cc56622c4810b5d7acd5ab2a
_Zodiac01 <lucasmasterls649@gmail.com>
2026-04-02 19:13:23 -0300

    mt bom

---
+++ calculadora que nn funciona.py
@@ -0,0 +1,6 @@
+num1 = (input("digite um numero: "))
+num2 = (input("digite outro numero: "))
+print("\nsoma dos dois:", num1 + num2)
+print("\nespero que tenha gostado, deixe seu feedback positivo e dê cinco estrelas\n")
+
+#KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK
